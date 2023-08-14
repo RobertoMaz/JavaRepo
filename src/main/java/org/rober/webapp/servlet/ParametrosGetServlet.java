1483708d02e6389402f56d387ab626de2a0e782d
Roberto Mazzarello <rjmazzarello@gmail.com>
2023-08-14 17:57:05 -0300

    commiteando

---
+++ src/main/java/org/rober/webapp/servlet/ParametrosGetServlet.java
@@ -0,0 +1,47 @@
+package org.rober.webapp.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/parametros/url-get")
+public class ParametrosGetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+
+        String saludo = req.getParameter("saludo");
+        String nombre = req.getParameter("nombre");
+
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("     <head>");
+        out.println("         <meta charset=\"UTF-8\">");
+        out.println("         <title>Parametros Get</title>");
+        out.println("     </head>");
+        out.println("     <body>");
+        out.println("         <h1>Parametros de GET de la URL</h1>");
+        if(saludo != null && nombre != null){
+            out.println("         <h2>El saludo enviado es: " + saludo + " " + nombre + "</h2>");
+
+        } else if (saludo != null) {
+
+            out.println("         <h2>El saludo enviado es: " + saludo + "</h2>");
+        } else {
+            out.println("<h3>No se han pasado los parametros saludo y nombre</h3>");
+        }
+        out.println("<a href=\"/webapp\">Home</a>");
+        out.println("     </body>");
+        out.println("</html>");
+        out.close();
+    }
+}
